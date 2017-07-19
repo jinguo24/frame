e33b89940628d67a0d45798c5d8d5001ce98871f
zhengfy1 <zhengfy1@lenovo.com>
2017-07-19 15:51:30 +0800

    update

--- simple-project-yuntongxun/src/main/java/com/cloopen/rest/demo/SDKTestSendTemplateSMS.java
+++ simple-project-yuntongxun/src/main/java/com/cloopen/rest/demo/SDKTestSendTemplateSMS.java
@@ -60,5 +60,29 @@ public class SDKTestSendTemplateSMS {
 			throw new Exception("sendmsg error:"+StringUtils.trimToEmpty(msg));
 		}
 	}
+	
+	public static void sendSms(String phone,String appId,String templateId,String[] params) throws Exception{
+		HashMap<String, Object> result = null;
+		CCPRestSDK restAPI = new CCPRestSDK();
+		restAPI.init("app.cloopen.com", "8883");// 初始化服务器地址和端口，格式如下，服务器地址不需要写https://
+		restAPI.setAccount(EnvPropertiesConfiger.getValue("ytx_account_sid"), EnvPropertiesConfiger.getValue("ytx_auth_token"));// 初始化主帐号和主帐号TOKEN
+		restAPI.setAppId(appId);// 初始化应用ID
+		result = restAPI.sendTemplateSMS(phone,templateId ,params);
+		//System.out.println("SDKTestSendTemplateSMS result=" + result);
+		if("000000".equals(result.get("statusCode"))){
+			//正常返回输出data包体信息（map）
+			//HashMap<String,Object> data = (HashMap<String, Object>) result.get("data");
+			//Set<String> keySet = data.keySet();
+			//for(String key:keySet){
+			//	Object object = data.get(key);
+			//	System.out.println(key +" = "+object);
+			//}
+		}else{
+			//异常返回输出错误码和错误信息
+			//System.out.println("错误码=" + result.get("statusCode") +" 错误信息= "+result.get("statusMsg"));
+			String msg = (String) result.get("statusMsg");
+			throw new Exception("sendmsg error:"+StringUtils.trimToEmpty(msg));
+		}
+	}
 
 }
